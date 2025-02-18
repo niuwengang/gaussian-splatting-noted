be594a688606a64546d8eb71e8440c8e3db96241
牛文港 <niuwengang1@xiaomi.com>
2025-02-18 14:06:02 +0800

    计算协方差矩阵

--- submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
+++ submodules/diff-gaussian-rasterization/cuda_rasterizer/forward.cu
@@ -113,32 +113,38 @@ __device__ float3 computeCov2D(const float3& mean, float focal_x, float focal_y,
 // of quaternion normalization.
 __device__ void computeCov3D(const glm::vec3 scale, float mod, const glm::vec4 rot, float* cov3D)
 {
-	// Create scaling matrix
+  // Create scaling matrix
+  //缩放矩阵
 	glm::mat3 S = glm::mat3(1.0f);
 	S[0][0] = mod * scale.x;
 	S[1][1] = mod * scale.y;
 	S[2][2] = mod * scale.z;
 
-	// Normalize quaternion to get valid rotation
+    // Normalize quaternion to get valid rotation
+    // 提取四元数
 	glm::vec4 q = rot;// / glm::length(rot);
 	float r = q.x;
 	float x = q.y;
 	float y = q.z;
 	float z = q.w;
 
-	// Compute rotation matrix from quaternion
+    // Compute rotation matrix from quaternion
+    //用四元数分量构造旋转矩阵
 	glm::mat3 R = glm::mat3(
 		1.f - 2.f * (y * y + z * z), 2.f * (x * y - r * z), 2.f * (x * z + r * y),
 		2.f * (x * y + r * z), 1.f - 2.f * (x * x + z * z), 2.f * (y * z - r * x),
 		2.f * (x * z - r * y), 2.f * (y * z + r * x), 1.f - 2.f * (x * x + y * y)
 	);
-
+	
+    //加上缩放矩阵
 	glm::mat3 M = S * R;
 
 	// Compute 3D world covariance matrix Sigma
+	//计算协方差矩阵
 	glm::mat3 Sigma = glm::transpose(M) * M;
 
-	// Covariance is symmetric, only store upper right
+    // Covariance is symmetric, only store upper right
+	//协方差矩阵对称，提取紧凑6参数
 	cov3D[0] = Sigma[0][0];
 	cov3D[1] = Sigma[0][1];
 	cov3D[2] = Sigma[0][2];
